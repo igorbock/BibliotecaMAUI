0ea1cc74f4a763e6ffececa50de4e29f85212570
Igor-bock <igor@glasscad.com.br>
2023-03-03 17:42:54 -0300

    Adicionando constantes para as mensagens de solicitação.

---
+++ BibliotecaMAUI/Constantes/MensagensSolicitacoes.cs
@@ -0,0 +1,9 @@
+﻿namespace BibliotecaMAUI.Constantes
+{
+    public static class MensagensSolicitacoes
+    {
+        public const string C_EditarEntidade = "A entidade da classe \"{0}\" foi editada corretamente.\n\nJSON:\n{1}";
+        public const string C_CriarEntidade = "A entidade da classe \"{0}\" foi criada corretamente.\n\nJSON:\n{1}";
+        public const string C_RemoverEntidade = "A entidade da classe \"{0}\" foi removida corretamente.";
+    }
+}
