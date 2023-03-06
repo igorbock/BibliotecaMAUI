4bed126afa70c9ac20f983ec6283ca0ded718f7a
Igor-bock <igor@glasscad.com.br>
2023-03-06 07:19:14 -0300

    Finalizando testes com mock

---
+++ BibliotecaMAUI/Constantes/MensagensExceptions.cs
@@ -0,0 +1,8 @@
+﻿namespace BibliotecaMAUI.Constantes
+{
+    public static class MensagensExceptions
+    {
+        public const string C_DesserializarSolicitacao = "Ocorreu um erro ao desserializar a solicitação. Revise o JSON enviado.";
+        public const string C_EntidadeNaoEncontrada = "A entidade \"{0}\" não está na base de dados e não foi possível encontrar registros";
+    }
+}
